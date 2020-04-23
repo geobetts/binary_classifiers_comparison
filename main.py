ce5cb41b21700922af6c1a133620f6f7a912bc56
geobetts <geo.bettsworth@gmail.com>
2020-04-23 18:21:14 +0100

    pca randomized svd

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ import research_pipeline as rp
 data_location = r"../binary_classifiers_comparison_data"
 output_location = r"../binary_classifiers_comparison_outputs"
 
+file_suffix = "pca_randomized_svd_50_comps"
+
 random.seed(123)
 
 array_dictionary_keys = ['a_wh', 'a_yn', 'b_wh', 'b_yn']
@@ -34,23 +36,25 @@ for name in array_dictionary_keys:
 # principle components analysis to create another feature representation
 # how do you choose number of components?
 
-tsne_fits = {}
+fits = {}
+info = {}
 
 for name in array_dictionary_keys:
-    print(f"{name} tsne")
-
-    tsne = TSNE(n_components=3)
+    print(f"{name} fit")
+    components= 50
+    pca = PCA(n_components=components, svd_solver='randomized')
     scaled = StandardScaler().fit_transform(arrays[name])
-    tsne_fit = tsne.fit_transform(scaled)
-    tsne_fits[name + '_tsne'] = tsne_fit
+    fit = pca.fit_transform(scaled)
+    fits[name + '_fit'] = fit
+    info[name] = pca.explained_variance_ratio_
 
 # might be interesting to include bothe pca and tsne and talk about differences
-# with open(r"../binary_classifiers_comparison_outputs/pca_analysis.csv", 'w') as f:
-#     writer = csv.writer(f)
-#     for row in pca_info.items():
-#         writer.writerow(row)
+with open(rf"../binary_classifiers_comparison_outputs/pca_analysis_{components}.csv", 'w') as f:
+    writer = csv.writer(f)
+    for row in info.items():
+        writer.writerow(row)
 
-arrays.update(tsne_fits)
+arrays.update(fits)
 
 # now there are the original versions of each one plus the pca versions
 
@@ -58,11 +62,11 @@ arrays.update(tsne_fits)
 
 performances = pd.DataFrame()
 
-trains = ['a_wh', 'a_yn', 'b_wh', 'b_yn', 'a_wh_tsne', 'a_yn_tsne', 'b_wh_tsne', 'b_yn_tsne'] * 2
+trains = ['a_wh', 'a_yn', 'b_wh', 'b_yn', 'a_wh_fit', 'a_yn_fit', 'b_wh_fit', 'b_yn_fit'] * 2
 
 train_targets = ['a_wh', 'a_yn', 'b_wh', 'b_yn'] * 4
 
-tests = ['b_wh', 'b_yn', 'a_wh', 'a_yn', 'b_wh_tsne', 'b_yn_tsne', 'a_wh_tsne', 'a_yn_tsne'] * 2
+tests = ['b_wh', 'b_yn', 'a_wh', 'a_yn', 'b_wh_fit', 'b_yn_fit', 'a_wh_fit', 'a_yn_fit'] * 2
 
 test_targets = ['b_wh', 'b_yn', 'a_wh', 'a_yn'] * 4
 
@@ -86,4 +90,4 @@ for (train_set, train_targets, test_set, test_targets, model) in zip(trains, tra
 performances.columns = ['train_set', 'test_set', 'model', 'best_performances', 'chosen_performance', 'cv_accuracy_best',
                         'accuracy_ratio', 'confusion_matrix', 'precision', 'recall', 'f1', 'time']
 
-performances.to_csv(r"../binary_classifiers_comparison_outputs/model_performances.csv", index=False)
+performances.to_csv(rf"../binary_classifiers_comparison_outputs/model_performances_{file_suffix}.csv", index=False)
