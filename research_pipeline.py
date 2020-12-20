92d8b368006f4e5eae38b7f13794f8921accefe2
geobetts <geo.bettsworth@gmail.com>
2020-12-20 21:01:46 +0000

    new pipeline

--- research_pipeline.py
+++ research_pipeline.py
@@ -3,94 +3,94 @@ Author: G Bettsworth
 """
 
 import numpy as np
-import pandas as pd
-import random
-import time
-import sklearn.metrics
+from random import seed
+from time import time
+from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import cross_validate
-import tensorflow as tf
-from tensorflow import keras
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.dummy import DummyClassifier
+
+from pandas import DataFrame, Series
+
+data_location = r"../binary_classifiers_comparison_data"
+output_location = r"../binary_classifiers_comparison_outputs"
+
+seed(123)
+
+array_a = np.genfromtxt(rf"{data_location}/a_wh_question_datapoints.txt", skip_header=1)
+target_a = np.genfromtxt(rf"{data_location}/a_wh_question_targets.txt")
+
+array_b = np.genfromtxt(rf"{data_location}/b_wh_question_datapoints.txt", skip_header=1)
+target_b = np.genfromtxt(rf"{data_location}/b_wh_question_targets.txt")
+
+classifiers = [DummyClassifier(), DecisionTreeClassifier(), KNeighborsClassifier(), RandomForestClassifier(),
+               MLPClassifier(), SVC(), AdaBoostClassifier(), GaussianProcessClassifier()]
+scaler = StandardScaler()
+train_set = array_a
+test_set = array_b
+train_targets = target_a
+test_targets = target_b
 
 
-def sklearn_prediction_pipeline(train_set,
-                            train_targets,
-                            test_set,
-                            test_targets,
-                            model):
-        
-    t = time.time()
-    
-    print('--------------------------------------')
-    print(f'Model {model}')
+class GridSearchClassifier:
+    """
+    """
 
-    train_set = StandardScaler().fit_transform(train_set)
-    test_set = StandardScaler().fit_transform(test_set)
-    
-    
-    performances = cross_validate(estimator=model.fit(train_set, train_targets), 
-                                  X=train_set, 
-                                  y=train_targets, 
-                                  return_estimator=True)
+    def __init__(self, train_set, test_set, train_targets, test_targets, scaler, classifiers):
+        """
+        """
 
-    random.seed(123)
+        self.train_set = train_set
+        self.test_set = test_set
+        self.train_targets = train_targets
+        self.test_targets = test_targets
+        self.scaler = scaler
+        self.classifiers = classifiers
 
-    model_selected = random.choice(performances['estimator'])
+    def _pipeline(self, scaler, model):
+        """
+        """
 
-    neighbours = model_selected
+        t = time()
 
-    knn_fit = neighbours.fit(train_set, train_targets)
+        fitted_scaler = scaler.fit(self.train_set)
+        train_set = fitted_scaler.transform(self.train_set)
+        test_set = fitted_scaler.transform(self.test_set)
 
-    predictions = knn_fit.predict(test_set)
+        fitted_model = model.fit(X=train_set, y=train_targets)
+        predictions = fitted_model.predict(test_set)
 
-    predictions = predictions.reshape(test_targets.shape)
-    
-    accuracy_ratio = sklearn.metrics.accuracy_score(y_true=test_targets, y_pred=predictions)
+        score = accuracy_score(y_true=self.test_targets, y_pred=predictions)
+        final_time = time() - t
 
-    conf_matrix = sklearn.metrics.confusion_matrix(y_true=test_targets, y_pred=predictions, labels=[1, 0])
-    precision_value = sklearn.metrics.precision_score(y_true=test_targets, y_pred=predictions)
-    recall_value = sklearn.metrics.recall_score(y_true=test_targets, y_pred=predictions)
-    f1 = sklearn.metrics.f1_score(y_true=test_targets, y_pred=predictions)
+        return score, final_time
 
-    t2 = time.time() - t
-    
-    outputs = {'model' : str(model),
-               'cross_validation': str(performances),
-               'accuracy' : accuracy_ratio,
-               'conf_matrix' : conf_matrix,
-               'precision_value' : precision_value,
-               'recall_value' : recall_value,
-               'f1' : f1,
-               't2' : t2}
+    def fit(self):
 
-    return outputs
+        classifier_strings = [str(x) for x in classifiers]
 
+        df = DataFrame(columns=['accuracy', 'time'], index=classifier_strings)
 
+        for classifier in self.classifiers:
+            print("--------------------------------------")
+            print(f'Testing: {classifier}')
+            score, time = self._pipeline(scaler=self.scaler, model=classifier)
 
-def tenserflow_prediction_pipeline(train_set,
-                                train_targets,
-                                test_set,
-                                test_targets):
-    
-    print('tenserflow pipeline')
+            print(f'Accuracy: {score}')
+            print(f'Time: {time}')
+            print("--------------------------------------")
 
-    model = keras.Sequential([
-            keras.layers.Dense(128, activation='relu'),
-            keras.layers.Dense(10)
-    ])
-    
-    
-    model.compile(optimizer='RMSprop',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-    
-    
-    model.fit(train_set, train_targets, epochs=1000)
-    
-    test_loss, test_acc = model.evaluate(test_set, test_targets, verbose=2)
-    
-    return test_acc
+            df.loc[str(classifier)] = Series({'accuracy': score, 'time': time})
+
+        return df
 
 
+output = GridSearchClassifier(train_set, test_set, train_targets, test_targets, scaler, classifiers).fit()
 
 
