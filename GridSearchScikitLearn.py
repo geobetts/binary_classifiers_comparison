5b78dccac856d3e6e83a47051e82a955e9df82cf
geobetts <geo.bettsworth@gmail.com>
2020-12-22 15:16:50 +0000

    Updated script with error logging and docstring.

--- GridSearchScikitLearn.py
+++ GridSearchScikitLearn.py
@@ -1,8 +1,11 @@
 """
+GridSearchScikitLearn
+
 Author: G Bettsworth
+
+2020
 """
 
-import numpy as np
 from random import seed
 from time import time
 from sklearn.metrics import accuracy_score
@@ -18,33 +21,63 @@ from sklearn.dummy import DummyClassifier
 
 from pandas import DataFrame, Series
 
+from numpy import ndarray, genfromtxt
+
 data_location = r"../binary_classifiers_comparison_data"
 output_location = r"../binary_classifiers_comparison_outputs"
 
 seed(123)
 
-array_a = np.genfromtxt(rf"{data_location}/a_wh_question_datapoints.txt", skip_header=1)
-target_a = np.genfromtxt(rf"{data_location}/a_wh_question_targets.txt")
+array_a = genfromtxt(rf"{data_location}/a_wh_question_datapoints.txt", skip_header=1)
+target_a = genfromtxt(rf"{data_location}/a_wh_question_targets.txt")
 
-array_b = np.genfromtxt(rf"{data_location}/b_wh_question_datapoints.txt", skip_header=1)
-target_b = np.genfromtxt(rf"{data_location}/b_wh_question_targets.txt")
+array_b = genfromtxt(rf"{data_location}/b_wh_question_datapoints.txt", skip_header=1)
+target_b = genfromtxt(rf"{data_location}/b_wh_question_targets.txt")
 
-classifiers = [DummyClassifier(), DecisionTreeClassifier(), KNeighborsClassifier(), RandomForestClassifier(),
+classifiers = [DummyClassifier(strategy='most_frequent'), DecisionTreeClassifier(), KNeighborsClassifier(), RandomForestClassifier(),
                MLPClassifier(), SVC(), AdaBoostClassifier(), GaussianProcessClassifier()]
+
 scaler = StandardScaler()
 train_set = array_a
 test_set = array_b
 train_targets = target_a
 test_targets = target_b
 
+#TODO - add scaler error logging
+
 
 class GridSearchClassifier:
     """
+    Tool to try different Scikit-Learn classifiers for a given classification task.
+
+    Parameters
+    -----------
+    train_set: numpy.ndarray
+        Training set.
+    test_set: numpy.ndarray
+        Test set.
+    train_targets: numpy.ndarray
+        Training targets (categories). This should a single series.
+    test_targets: numpy.ndarray
+        Test targets (categories).
+    classifiers: list
+        List of Scikit-Learn classifiers.
+    scaler: , default=StandardScaler()
+        Scaler from the Scikit-Learn library.
     """
 
-    def __init__(self, train_set, test_set, train_targets, test_targets, scaler, classifiers):
+    def __init__(self, train_set, test_set, train_targets, test_targets, classifiers, scaler=StandardScaler()):
         """
+        Error logging performed and variables set.
         """
+
+        variables = [train_set, test_set, train_targets, test_targets, classifiers]
+        variable_names = ['train_set', 'test_set', 'train_targets', 'test_targets', 'classifiers']
+        types = [ndarray, ndarray, ndarray, ndarray, list]
+
+        for variable, name, variable_type in zip(variables, variable_names, types):
+            if not isinstance(variable, variable_type):
+                raise TypeError(f"The variable {name} needs to be a {variable_type}")
 
         self.train_set = train_set
         self.test_set = test_set
@@ -88,9 +121,12 @@ class GridSearchClassifier:
 
             df.loc[str(classifier)] = Series({'accuracy': score, 'time': time})
 
+            df['ranks'] = df['accuracy'].rank(ascending=False) + df['time'].rank()
+            df = df.sort_values(by=['ranks'])
+
         return df
 
 
-output = GridSearchClassifier(train_set, test_set, train_targets, test_targets, scaler, classifiers).fit()
+output = GridSearchClassifier(train_set, test_set, train_targets, test_targets, classifiers).fit()
 
 
